984cb01622dbb9e40d97556361724b6caa4063e2
User <User@ADMINs-MacBook-Pro.local>
2019-02-19 09:58:04 -0500

    Async assignments done.

---
+++ WhoWroteItLoader/app/src/main/java/com/example/user/whowroteit/BookLoader.java
@@ -0,0 +1,30 @@
+package com.example.user.whowroteit;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.AsyncTaskLoader;
+
+public class BookLoader extends AsyncTaskLoader<String> {
+    public BookLoader(@NonNull Context context) {
+        super(context);
+    }
+
+    @Nullable
+    @Override
+    public String loadInBackground() {
+        return NetworkUtils.getBookInfo(mQueryString);
+
+    }
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+    private String mQueryString;
+
+    BookLoader(Context context, String queryString) {
+        super(context);
+        mQueryString = queryString;
+    }
+}
